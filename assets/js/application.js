a0baebb319fb4d84530a2e5bd53cc5a353f55bec
caiguanhao <caiguanhao@gmail.com>
2013-10-16 17:10:01 +0800

    add slider, side menu and cases;

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,1 +1,13 @@
 //= require vendor/jquery-1.10.2.min.js
+//= require vendor/jquery.sliderkit.1.9.2.js
+$(function(){
+  if ($('#main-slider').length == 1) {
+    $('#main-slider').sliderkit({
+      auto: true,
+      autospeed: 3000,
+      panelbtnshover: true,
+      circular: true,
+      fastchange: false
+    });
+  }
+});
