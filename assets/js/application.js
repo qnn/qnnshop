bb91a2c00b74d236e5ff8dc857d1560148b4cd21
caiguanhao <caiguanhao@gmail.com>
2013-10-25 13:58:22 +0800

    adding cart;

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,6 @@
 //= require vendor/jquery-1.10.2.min.js
 //= require vendor/jquery.sliderkit.1.9.2.js
+//= require vendor/simpleCart.min.js
 $(function(){
   if ($('#main-slider').length == 1) {
     $('#main-slider').sliderkit({
@@ -10,4 +11,41 @@ $(function(){
       fastchange: false
     });
   }
+  simpleCart({
+    checkout: {
+      type: "SendForm",
+      url: "/buy"
+    },
+    cartColumns: [
+      {
+        view: function (item, column) {
+          return "<span>" + item.get('quantity') + "</span>" +
+            "<div>" +
+            "<a href='javascript:;' class='simpleCart_increment'><img src='/assets/images/increment.png' title='+1' alt='arrow up'/></a>" +
+            "<a href='javascript:;' class='simpleCart_decrement'><img src='/assets/images/decrement.png' title='-1' alt='arrow down'/></a>" +
+            "</div>";
+        },
+        attr: 'custom'
+      },
+      {
+        attr: "name",
+        label: false
+      },
+      {
+        view: 'currency',
+        attr: "total",
+        label: false
+      }
+    ],
+    cartStyle: 'div'
+  });
+  $(".cartInfo").click(function(){
+    if ($(this).hasClass('open')) {
+      $("#cartPopover").hide();
+      $(this).removeClass('open');
+    } else {
+      $("#cartPopover").show();
+      $(this).addClass('open');
+    }
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@ module.exports = function(app, products) {
     res.render('index');
   });
 
+  app.post('/cart', function(req, res, next){
+    var category = req.body.category, model = req.body.model;
+    if (category && model && products[category] && products[category][model]) {
+      res.render('products/show', { products: products, category: category, model: model });
+    } else {
+      res.send(500);
+    }
+  });
+
   app.get('/:category/:model', function(req, res, next){
     var category = req.params.category, model = req.params.model;
     if (products[category] && products[category][model]) {
