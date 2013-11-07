00606ebdf1e241ce6952dde8178d683d15e8c855
caiguanhao <caiguanhao@gmail.com>
2013-11-07 20:23:41 +0800

    fixing interfaces;

--- assets/js/vendor/jquery.timeago.js
+++ assets/js/vendor/jquery.timeago.js
@@ -195,8 +195,8 @@
 // Simplified Chinese
 jQuery.timeago.settings.strings = {
   prefixAgo: null,
-  prefixFromNow: "从现在开始",
-  suffixAgo: "之前",
+  prefixFromNow: "刚刚",
+  suffixAgo: "前",
   suffixFromNow: null,
   seconds: "不到 1 分钟",
   minute: "大约 1 分钟",
