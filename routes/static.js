2b02825f97964c8bf908647e6028835630f6940f
caiguanhao <caiguanhao@gmail.com>
2013-12-21 19:34:31 +0800

    static pages added;

--- routes/static.js
+++ routes/static.js
@@ -1,3 +1,7 @@
 module.exports = function(app, products, configs) {
-  
+
+  app.get('/:static(about|contact|tips|choose)', function(req, res){
+    res.render('static/' + req.params.static, { belongs_to: req.params.static });
+  });
+
 };
